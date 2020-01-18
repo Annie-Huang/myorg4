467fc6dfcb2a71b5f56d687e13e472c09748a3d1
Annie Huang <annie.hn@gmail.com>
2020-01-19 10:31:25 +1100

    Update main.ts so backend api provided data.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,9 +7,16 @@ import * as express from 'express';
 
 const app = express();
 
+interface Todo {
+  title: string;
+}
+
+const todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
+app.get('/api/todos', (req, resp) => resp.send(todos));
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
